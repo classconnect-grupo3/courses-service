6526caecc596dbc95e33f9ccbe8327b014ef068d
rafaortegano <rortegano@fi.uba.ar>
2025-05-18 19:14:07 -0300

    feat: add Assignment model with basic fields and validations

---
+++ src/model/assignment.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Assignment struct {
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Title       string            `json:"title" bson:"title"`
+	Description string            `json:"description" bson:"description"`
+	CourseID    string            `json:"course_id" bson:"course_id"`
+	DueDate     time.Time         `json:"due_date" bson:"due_date"`
+	CreatedAt   time.Time         `json:"created_at" bson:"created_at"`
+	UpdatedAt   time.Time         `json:"updated_at" bson:"updated_at"`
+} 
