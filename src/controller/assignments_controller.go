ccab1278280f2f35f644f4dc4a5d50cfba2392a9
rafaortegano <rortegano@fi.uba.ar>
2025-05-18 19:19:26 -0300

    feat: add assignments controller with create, read, update and delete operations

---
+++ src/controller/assignments_controller.go
@@ -0,0 +1,134 @@
+package controller
+
+import (
+	"log/slog"
+	"net/http"
+
+	"courses-service/src/model"
+	"courses-service/src/schemas"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AssignmentService interface {
+	CreateAssignment(c schemas.CreateAssignmentRequest) (*model.Assignment, error)
+	GetAssignments() ([]*model.Assignment, error)
+	GetAssignmentById(id string) (*model.Assignment, error)
+	GetAssignmentsByCourseId(courseId string) ([]*model.Assignment, error)
+	UpdateAssignment(id string, updateAssignmentRequest schemas.UpdateAssignmentRequest) (*model.Assignment, error)
+	DeleteAssignment(id string) error
+}
+
+type AssignmentsController struct {
+	service AssignmentService
+}
+
+func NewAssignmentsController(service AssignmentService) *AssignmentsController {
+	return &AssignmentsController{service: service}
+}
+
+func (c *AssignmentsController) GetAssignments(ctx *gin.Context) {
+	slog.Debug("Getting assignments")
+
+	assignments, err := c.service.GetAssignments()
+	if err != nil {
+		slog.Error("Error getting assignments", "error", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	slog.Debug("Assignments retrieved", "assignments", assignments)
+	ctx.JSON(http.StatusOK, assignments)
+}
+
+func (c *AssignmentsController) CreateAssignment(ctx *gin.Context) {
+	slog.Debug("Creating assignment")
+
+	var assignment schemas.CreateAssignmentRequest
+	if err := ctx.ShouldBindJSON(&assignment); err != nil {
+		slog.Error("Error binding JSON", "error", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	createdAssignment, err := c.service.CreateAssignment(assignment)
+	if err != nil {
+		slog.Error("Error creating assignment", "error", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	slog.Debug("Assignment created", "assignment", createdAssignment)
+	ctx.JSON(http.StatusCreated, createdAssignment)
+}
+
+func (c *AssignmentsController) GetAssignmentById(ctx *gin.Context) {
+	slog.Debug("Getting assignment by ID")
+	id := ctx.Param("id")
+
+	assignment, err := c.service.GetAssignmentById(id)
+	if err != nil {
+		slog.Error("Error getting assignment", "error", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if assignment == nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "assignment not found"})
+		return
+	}
+
+	slog.Debug("Assignment retrieved", "assignment", assignment)
+	ctx.JSON(http.StatusOK, assignment)
+}
+
+func (c *AssignmentsController) GetAssignmentsByCourseId(ctx *gin.Context) {
+	slog.Debug("Getting assignments by course ID")
+	courseId := ctx.Param("courseId")
+
+	assignments, err := c.service.GetAssignmentsByCourseId(courseId)
+	if err != nil {
+		slog.Error("Error getting assignments", "error", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	slog.Debug("Assignments retrieved", "assignments", assignments)
+	ctx.JSON(http.StatusOK, assignments)
+}
+
+func (c *AssignmentsController) UpdateAssignment(ctx *gin.Context) {
+	slog.Debug("Updating assignment")
+	id := ctx.Param("id")
+
+	var updateAssignmentRequest schemas.UpdateAssignmentRequest
+	if err := ctx.ShouldBindJSON(&updateAssignmentRequest); err != nil {
+		slog.Error("Error binding JSON", "error", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedAssignment, err := c.service.UpdateAssignment(id, updateAssignmentRequest)
+	if err != nil {
+		slog.Error("Error updating assignment", "error", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	slog.Debug("Assignment updated", "assignment", updatedAssignment)
+	ctx.JSON(http.StatusOK, updatedAssignment)
+}
+
+func (c *AssignmentsController) DeleteAssignment(ctx *gin.Context) {
+	slog.Debug("Deleting assignment")
+	id := ctx.Param("id")
+
+	if err := c.service.DeleteAssignment(id); err != nil {
+		slog.Error("Error deleting assignment", "error", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	slog.Debug("Assignment deleted")
+	ctx.JSON(http.StatusNoContent, nil)
+} 
