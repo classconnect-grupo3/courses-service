a5cc0ddfd6403d452724a928775348c6a171c488
rovifran <frovira@fi.uba.ar>
2025-06-08 18:41:18 -0300

    feat: completed prototype of ai model response

--- src/ai/ai_client.go
+++ src/ai/ai_client.go
@@ -2,6 +2,8 @@ package ai
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"courses-service/src/model"
@@ -10,13 +12,26 @@ import (
 )
 
 type AiClient struct {
+	context      context.Context
 	GeminiApiKey string
 	Client       *genai.Client
 }
 
-// const aiModel = "gemini-2.0-flash"
+const aiModel = "gemini-2.0-flash"
+
+func generateCourseFeedbacksPrompt(feedbacks []model.CourseFeedback) string {
+	prompt := SummarizeCourseFeedbacksPrompt
+	for _, feedback := range feedbacks {
+		prompt += fmt.Sprintf("Puntuacion: %d\n", feedback.Score)
+		prompt += fmt.Sprintf("Tipo: %s\n", feedback.FeedbackType)
+		prompt += fmt.Sprintf("Feedback: %s\n", feedback.Feedback)
+	}
+	return prompt
+}
+
 func NewAiClient(geminiApiKey string) *AiClient {
-	client, err := genai.NewClient(context.Background(), &genai.ClientConfig{
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{
 		APIKey: geminiApiKey,
 	})
 	if err != nil {
@@ -25,11 +40,27 @@ func NewAiClient(geminiApiKey string) *AiClient {
 	}
 	log.Println("Gemini client created")
 	return &AiClient{
+		context:      ctx,
 		GeminiApiKey: geminiApiKey,
 		Client:       client,
 	}
 }
 
-func (c *AiClient) SummarizeCourseFeedbacks(feedbacks []model.CourseFeedback) string {
-	return ""
+func (c *AiClient) SummarizeCourseFeedbacks(feedbacks []model.CourseFeedback) {
+	prompt := generateCourseFeedbacksPrompt(feedbacks)
+	response, err := c.Client.Models.GenerateContent(c.context, aiModel, genai.Text(prompt), nil)
+	if err != nil {
+		log.Fatal("Failed to generate content", err)
+	}
+	debugPrint(response)
+}
+
+func debugPrint[T any](r *T) {
+
+	response, err := json.MarshalIndent(*r, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(response))
 }
