b48ce6f26e916fefc041c24637e6fa31de7d33c9
rovifran <frovira@fi.uba.ar>
2025-06-15 12:09:07 -0300

    feat: inital declaration of queue

---
+++ src/router/queues.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"fmt"
+	"os"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func CreateNotificationsQueue() (*amqp.Channel, error) {
+	queueName := os.Getenv("NOTIFICATIONS_QUEUE_NAME")
+	queueURL := os.Getenv("RABBITMQ_URL")
+
+	conn, err := amqp.Dial(queueURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to RabbitMQ: %w", err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open a channel: %w", err)
+	}
+
+	_, err = ch.QueueDeclare(
+		queueName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to declare queue: %w", err)
+	}
+
+	return ch, nil
+}
