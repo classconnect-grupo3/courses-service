15e91ed10f279b3fd6394962f3fcf795d3103ed1
rovifran <frovira@fi.uba.ar>
2025-06-23 21:30:14 -0300

    feat: modified model of enrollment to include reason for dropped enrollment

--- src/model/enrollment.go
+++ src/model/enrollment.go
@@ -7,15 +7,16 @@ import (
 )
 
 type Enrollment struct {
-	ID            primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	StudentID     string             `json:"student_id" bson:"student_id"`
-	CourseID      string             `json:"course_id" bson:"course_id"`
-	EnrolledAt    time.Time          `json:"enrolled_at" bson:"enrolled_at"`
-	CompletedDate time.Time          `json:"completed_date" bson:"completed_date"`
-	Status        EnrollmentStatus   `json:"status" bson:"status"`
-	Favourite     bool               `json:"favourite" bson:"favourite"`
-	UpdatedAt     time.Time          `json:"updated_at" bson:"updated_at"`
-	Feedback      []StudentFeedback  `json:"feedback" bson:"feedback"`
+	ID                    primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	StudentID             string             `json:"student_id" bson:"student_id"`
+	CourseID              string             `json:"course_id" bson:"course_id"`
+	EnrolledAt            time.Time          `json:"enrolled_at" bson:"enrolled_at"`
+	CompletedDate         time.Time          `json:"completed_date" bson:"completed_date"`
+	Status                EnrollmentStatus   `json:"status" bson:"status"`
+	Favourite             bool               `json:"favourite" bson:"favourite"`
+	UpdatedAt             time.Time          `json:"updated_at" bson:"updated_at"`
+	Feedback              []StudentFeedback  `json:"feedback" bson:"feedback"`
+	ReasonForUnenrollment string             `json:"reason_for_unenrollment,omitempty" bson:"reason_for_unenrollment,omitempty"`
 }
 
 type EnrollmentStatus string
